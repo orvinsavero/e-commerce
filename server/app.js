c20daadea13d339fcb74f93fe62c16ca2616a1ae
Orvin Savero <orvinsavero@yahoo.com>
2019-07-03 15:59:12 +0700

    deploy

--- server/app.js
+++ server/app.js
@@ -7,8 +7,8 @@ const app = express()
 const errorHandler = require('./helpers/error-handler.js')
 
 let local = 'mongodb://localhost/e-commerce-' + process.env.NODE_ENV 
-
-mongoose.connect(local, {useNewUrlParser: true})
+let uri = 'mongodb+srv://admin:admin@cluster0-ezblw.gcp.mongodb.net/ecommerce?retryWrites=true&w=majority'
+mongoose.connect(uri, {useNewUrlParser: true})
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
